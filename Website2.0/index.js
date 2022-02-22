feeadd6d56632b199d82b32841d22d28940856b4
Haydenchung03 <haydenchung21@hotmail.com>
2022-02-21 23:59:01 -0500

    changes

--- Website2.0/index.js
+++ Website2.0/index.js
@@ -1,28 +1,3 @@
-// Part 1
-// Sends an alert
-//alert("You are not the lover");
 
-// Types the message on the console
-//console.log("Hi");
-
-//let (Limited Scope)
-// const (Constant reference to a value that cannot change)
-// var (Variables name)
-
-// this is a comment
-
-/*
-
-This is a multi comment
-
-*/
-
-var userName = "Hayden"
-
-var totalUser = 10;
-
-
-console.log("This is the webpage of",userName);
-console.log("This is the number of friends that you have", totalUser);
 
 
